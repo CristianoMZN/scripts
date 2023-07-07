0539a816abf1a74df949af92d1e07595366b3a46
Cristiano Mozena <past.kun@gmail.com>
2023-07-07 11:26:10 -0300

    NodeJS 18 LTS Script Update

--- install_nodejs18LTS_ubuntu_22.sh
+++ install_nodejs18LTS_ubuntu_22.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install -y curl
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
